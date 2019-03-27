84bc8d077d187e851098811080aae0c339003cf4
mpoudret <31647377+mpoudret@users.noreply.github.com>
2019-03-27 19:18:34 +0100

    WITSML 2.0 perforation improvement (#123)
    - fix cs example
    - Fix missing WellboreObject namespace declaration in swig file

--- cmake/Program.cs
+++ cmake/Program.cs
@@ -13,7 +13,7 @@ namespace example
     {
         private static void serialize()
         {
-            using (EpcDocument epc_file = new EpcDocument("../../../TestingPackageCs"))
+            using (EpcDocument epc_file = new EpcDocument("../../../TestingPackageCs.epc"))
             {
                 LocalDepth3dCrs crs = epc_file.createLocalDepth3dCrs(Guid.NewGuid().ToString(), "UTF8 Crs title : éàç : олег1", 0.0, 0.0, 0.0, 0.0, eml20__LengthUom.eml20__LengthUom__m, 5215, eml20__LengthUom.eml20__LengthUom__m, "Unknown", false);
                 System.Console.WriteLine("Serialize : CRS title is " + crs.getTitle());
@@ -26,7 +26,7 @@ namespace example
 
         private static void deserialize()
         {
-            using (EpcDocument epc_file = new EpcDocument("../../../TestingPackageCs"))
+            using (EpcDocument epc_file = new EpcDocument("../../../TestingPackageCs.epc"))
             {
                 string status = epc_file.deserialize();
                 if (status.Length > 0) {
@@ -34,7 +34,7 @@ namespace example
 				}
                 LocalDepth3dCrsVector crs_set = epc_file.getLocalDepth3dCrsSet();
                 System.Console.WriteLine("Deserialize : CRS title is " + crs_set[0].getTitle());
-				Well well = epc_file.getResqmlAbstractObjectByUuid("1425632e-3c22-4845-b431-ecd36da0671e") as Well;
+				Well well = epc_file.getDataObjectByUuid("1425632e-3c22-4845-b431-ecd36da0671e") as Well;
                 System.Console.WriteLine("Deserialize : Well title is " + well.getTitle());
             }
         }
