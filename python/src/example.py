1e45e8fd441f505d33fa6400b948f3c104745e71
Philippe Verney <philippe.verney@f2i-consulting.com>
2021-12-10 14:59:38 +0100

    Add a property to partial grid in Python example

--- python/src/example.py
+++ python/src/example.py
@@ -9,13 +9,13 @@ import fesapi
 
 def serialize_grid(repo):
     """
-    This function create some grids in the repo to be serialized
+    This function creates a one cell grid + a property on a partial ijk grid in the repo to be serialized
     """
     earth_model = repo.createEarthModel("f2060ce0-fa3d-11e5-8620-0002a5d5c51b", "Grid")
     earth_model_interp = repo.createEarthModelInterpretation(
         earth_model, "f5cd7520-fa3d-11e5-b65b-0002a5d5c51b", "Grid interp")
 
-    # ONE SUGAR
+    # ONE CELL GRID
     single_cell_ijk_grid = repo.createIjkGridExplicitRepresentation(
         earth_model_interp, "e69bfe00-fa3d-11e5-b5eb-0002a5d5c51b", "One unfaulted sugar cube",
         1, 1, 1)
@@ -27,6 +27,16 @@ def serialize_grid(repo):
     single_cell_ijk_grid.setGeometryAsCoordinateLineNodes(
         fesapi.resqml20__PillarShape_vertical, fesapi.resqml20__KDirection_down, False,
         resqml_points)
+
+    # ONE CONTINUOUS PROPERTY ON A PARTIAL 3 cells I=1 J=1 K=3 GRID
+    partial_ijk_grid = repo.createPartialIjkGridRepresentation("bc92a216-aa17-4a1f-9253-8b3ab094bf84", "partial grid")
+    continuous_prop = repo.createContinuousProperty(partial_ijk_grid, "827d1074-5b16-4c7d-80bb-a8f3e1413753", "My length property", 1, fesapi.resqml22__IndexableElement_cells,
+        fesapi.resqml20__ResqmlUom_m, fesapi.resqml20__ResqmlPropertyKind_length)
+    prop_values = [1.1, 2.2, 3.3]
+    resqml_values = fesapi.FloatArray(3)
+    for i, value in enumerate(prop_values):
+        resqml_values.setitem(i, value)
+    continuous_prop.pushBackFloatHdf5Array3dOfValues(resqml_values, 1, 1, 3, 1.1, 3.3)
 
 def serialize(file_name):
     """
@@ -77,6 +87,8 @@ def show_ijk_grid(ijk_grid):
     This function shows some ijk grids which have been deserialized
     """
     show_all_metadata(ijk_grid)
+    if ijk_grid.isPartial():
+        return
 
     if ijk_grid.getGeometryKind() != fesapi.resqml2_AbstractIjkGridRepresentation.NO_GEOMETRY:
         most_complex_pillar_geom = ijk_grid.getMostComplexPillarGeometry()
