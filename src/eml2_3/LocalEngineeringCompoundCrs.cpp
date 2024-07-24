5ecb7418d51169e1f334115a5bef698967dc6de9
Philippe Verney <philippe.verney@f2i-consulting.com>
2024-07-24 12:14:58 +0200

    Allow boost version < 106600

--- src/eml2_3/LocalEngineeringCompoundCrs.cpp
+++ src/eml2_3/LocalEngineeringCompoundCrs.cpp
@@ -20,7 +20,12 @@ under the License.
 
 #include <stdexcept>
 
+#include <boost/version.hpp>
+#if BOOST_VERSION < 106600
 #include <boost/uuid/name_generator.hpp>
+#else
+#include <boost/uuid/name_generator_sha1.hpp>
+#endif
 #include <boost/uuid/uuid_io.hpp>
 
 #include "LocalEngineering2dCrs.h"
@@ -53,8 +58,18 @@ void LocalEngineeringCompoundCrs::init(COMMON_NS::DataObjectRepository * repo, c
 	eml23__LocalEngineeringCompoundCrs* local3dCrs = static_cast<eml23__LocalEngineeringCompoundCrs*>(gsoapProxy2_3);
 
 	// Uuid creation
-	boost::uuids::name_generator gen(boost::uuids::ns::dns());
+#if BOOST_VERSION < 106600
+	// According to https://datatracker.ietf.org/doc/html/rfc4122#appendix-C the DNS uuid is 6ba7b810-9dad-11d1-80b4-00c04fd430c8
+	uuid dnsUuid = { {
+		0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1 ,
+		0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8
+	}};
+	boost::uuids::name_generator gen(dnsUuid);
 	boost::uuids::name_generator finalGen(gen("f2i-consulting.com"));
+#else
+	boost::uuids::name_generator_sha1 gen(boost::uuids::ns::dns());
+	boost::uuids::name_generator_sha1 finalGen(gen("f2i-consulting.com"));
+#endif
 
 	// uom conversion
 	std::string str = gsoap_resqml2_0_1::soap_eml20__LengthUom2s(gsoapProxy2_3->soap, projectedUom);
